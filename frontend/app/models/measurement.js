a9a90d5987194c1059fe1482a4d42545e2a687e1
Patrick Baselier <patrick.baselier@gmail.com>
2016-03-31 08:24:03 +0200

    Extract date

--- frontend/app/models/measurement.js
+++ frontend/app/models/measurement.js
@@ -8,32 +8,36 @@ export default DS.Model.extend({
   username: attr('string'),
   happiness: attr('number'),
   createdAt: attr('date'),
+  adjustedCreatedAt: computed(function() {
+    // Entries submitted on Thursday still belong to the previous week.
+    let d  = new Date(this.get('createdAt').valueOf());
+    d.setDate(d.getDate() - 4);
+    return d;
+  }),
   year: computed(function() {
     return new Date(this.get('createdAt')).getFullYear();
   }),
   weekNo: computed(function() {
     // https://gist.github.com/dblock/1081513
     // Create a copy of this date object
-    let target  = new Date(this.get('createdAt').valueOf());
-    // Entries submitted on Thursday still belong to the previous week.
-    target.setDate(target.getDate() - 4);
+    let d  = new Date(this.get('adjustedCreatedAt').valueOf());
 
     // ISO week date weeks start on monday, so correct the day number
-    let dayNr   = (target.getDay() + 6) % 7;
+    let dayNr   = (d.getDay() + 6) % 7;
 
-    // Set the target to the thursday of this week so the
-    // target date is in the right year
-    target.setDate(target.getDate() - dayNr + 3);
+    // Set the d to the thursday of this week so the
+    // d date is in the right year
+    d.setDate(d.getDate() - dayNr + 3);
 
     // ISO 8601 states that week 1 is the week with january 4th in it
-    let jan4    = new Date(target.getFullYear(), 0, 4);
+    let jan4    = new Date(d.getFullYear(), 0, 4);
 
-    // Number of days between target date and january 4th
-    let dayDiff = (target - jan4) / 86400000;
+    // Number of days between d date and january 4th
+    let dayDiff = (d - jan4) / 86400000;
 
-    if(new Date(target.getFullYear(), 0, 1).getDay() < 5) {
+    if(new Date(d.getFullYear(), 0, 1).getDay() < 5) {
       // Calculate week number: Week 1 (january 4th) plus the
-      // number of weeks between target date and january 4th
+      // number of weeks between d date and january 4th
       return 1 + Math.ceil(dayDiff / 7);
     }
     else {  // jan 4th is on the next week (so next week is week 1)
