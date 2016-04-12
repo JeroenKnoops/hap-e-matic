876cbc06ca2c4fb20afa1177ada9e061a6ad3f12
Patrick Baselier <patrick.baselier@gmail.com>
2016-04-12 13:23:51 +0200

    Show more data and style

--- frontend/app/components/happiness-pie.js
+++ frontend/app/components/happiness-pie.js
@@ -1,27 +1,44 @@
 import Ember from 'ember';
 import PieChartDataElement from '../models/pie-chart-data-element';
 
-export default Ember.Component.extend({
-  chartData: Ember.computed('measurements.[]', function() {
+const { computed, Component } = Ember;
+
+export default Component.extend({
+  classNames: ['happiness-pie-container'],
+  chartData: computed('measurements.[]', function() {
     let result = [];
 
     this.get('measurements').forEach(function(measurement) {
-      let key = 'label',
+      let key = 'score',
           value = measurement.get('happiness'),
           hasGroup = !!result.findBy(key, value);
 
       if (!hasGroup) {
         result.pushObject(PieChartDataElement.create({
-          label: value
+          score: value
         }));
       }
 
       result.findBy(key, value).incrementProperty('value');
     });
 
-    return result.sortBy('label').map(function(item) {
+    return result.sortBy('score').map(function(item) {
       return item.getProperties('value', 'color', 'highlight', 'label');
     });
+  }),
+  weekNo: computed('measurements', function() {
+    return this.get('measurements.firstObject.weekNo');
+  }),
+  year: computed('measurements', function() {
+    return this.get('measurements.firstObject.year');
+  }),
+  count: computed('measurements', function() {
+    return this.get('measurements').length;
+  }),
+  scores: computed.mapBy('measurements', 'happiness'),
+  total: computed.sum('scores'),
+  happiness: computed('measurements', function() {
+    return Math.round(this.get('total')/this.get('count') * 10)/10;
   }),
   chartOptions: {
     segmentShowStroke: false
--- frontend/app/models/pie-chart-data-element.js
+++ frontend/app/models/pie-chart-data-element.js
@@ -1,19 +1,29 @@
 import Ember from 'ember';
 
-const { computed } = Ember;
+const { computed, Object } = Ember;
 
-export default Ember.Object.extend({
-  label: undefined,
-  value: null,
+export default Object.extend({
+  score: undefined,
+  value: undefined,
+  label: computed('score', function() {
+    return this.get('happinesses')[this.get('score')];
+  }),
   color: computed('hue', function() {
     return `hsl(${this.get('hue')}, 26%, 53%)`;
   }),
   highlight: computed('hue', function() {
-    return `hsl(${this.get('hue')}, 50%, 50%)`;
+    return `hsl(${this.get('hue')}, 46%, 53%)`;
   }),
-  hue: computed('label', function() {
-    return 30 * (this.get('label') - 1);
-  })
+  hue: computed('score', function() {
+    return 30 * (this.get('score') - 1);
+  }),
+  happinesses: {
+    '1': 'Very unhappy',
+    '2': 'Slightly depressed',
+    '3': 'Satisfactory',
+    '4': 'Convenient',
+    '5': 'Very happy'
+  }
 });
 
 
